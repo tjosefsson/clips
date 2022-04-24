2a9055d32a99414685a5f4a25db6e1a95833aba9
Thomas J <thomas.josefsson89@gmail.com>
2022-04-24 20:36:56 +0200

    start of reactive form

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)])
+  email = new FormControl('', [Validators.required])
+  age =  new FormControl('')
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber =  new FormControl('')
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+}
